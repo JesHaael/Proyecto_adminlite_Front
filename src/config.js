5ea1ca95b167451259c8b314227004d129564063
JesHaael <85897466+JesHaael@users.noreply.github.com>
2024-07-16 20:51:43 -0500

    fix config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     api:{
-      // baseURL: "http://localhost:5000"
+      //baseURL: "http://localhost:5000"
       // baseURL: "https://proyecto-adminlite-bak.onrender.com/"
-         baseURL: "https://proyecto-adminlite-bak-2.onrender.com/"
+         baseURL: "https://proyecto-adminlite-bak-2.onrender.com"
     }
 }
